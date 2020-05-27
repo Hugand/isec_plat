78367ebc02fc5d476e7c6b05d73d1d178d8fa575
Hugo Gomes <hugogomes@MBP-de-Hugo-2.lan>
2020-05-28 00:06:32 +0100

    Responsive design implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../css/Navbar.scss';
 import CadeiraNavbarItem from '../components/CadeiraNavbarItem'
+import { FiMenu } from "react-icons/fi";
+import { MdClear } from "react-icons/md";
 
 class Navbar extends React.Component{
 
@@ -67,7 +69,9 @@ class Navbar extends React.Component{
                     id: "dioji21s0c9",
                     nome: "Professor 4"
                 },
-            ]
+            ],
+            display_menu: false
+
         }
 
     }
@@ -77,96 +81,96 @@ class Navbar extends React.Component{
         disp[tab_name] = !disp[tab_name]
 
         this.setState({
-            display_tabs: disp
+            display_tabs: disp,
         })
     }
 
     render(){
         return (
-            <div id="navbar">
-                <ul className="outer-ul">
-                    <li onClick={()=>{window.location="/cadeiras"}}>Cadeiras</li>
-                        <ul className="year-ul">
-                            <li onClick={() => this.handle_tab('y1')}>1ºano</li>
-                            {(this.state.display_tabs.y1) ?
-                                <ul>
-                                    <ul className="semester-ul">
-                                        <li onClick={() => this.handle_tab('y1s1')}>1º semestre</li>
-                                        {(this.state.display_tabs.y1s1) ?
-                                        <ul className="inner-ul">
-                                            {this.state.cadeiras.ano_1.semestre_1.map(cadeira => 
-                                                <CadeiraNavbarItem cadeira={cadeira}/>)}
-                                        </ul>
-                                        : ""}
-                                    </ul>
-                                    <ul className="semester-ul">
-                                        <li onClick={() => this.handle_tab('y1s2')}>2º semestre</li>
-                                        {(this.state.display_tabs.y1s2) ?
-                                        <ul className="inner-ul">
-                                        {this.state.cadeiras.ano_1.semestre_2.map(cadeira => 
-                                            <CadeiraNavbarItem cadeira={cadeira}/>)}
-                                        </ul>
-                                        : ""}
-                                    </ul>
-                                </ul>
-                            : ""}
-                            
-                        </ul>
-                        <ul className="year-ul">
-                            <li onClick={() => this.handle_tab('y2')}>2ºano</li>
-                            {(this.state.display_tabs.y2) &&
-                                <ul>
-                                    <ul className="semester-ul">
-                                        <li onClick={() => this.handle_tab('y2s1')}>1º semestre</li>
-                                            {(this.state.display_tabs.y2s1) ?
-                                                <ul className="inner-ul">
-                                                {this.state.cadeiras.ano_2.semestre_1.map(cadeira => 
-                                                    <CadeiraNavbarItem cadeira={cadeira}/>)}
-                                                </ul>
-                                                : ""}
-                                    </ul>
-                                    <ul className="semester-ul">
-                                        <li onClick={() => this.handle_tab('y2s2')}>2º semestre</li>
-                                            {(this.state.display_tabs.y2s2) ?
-                                                <ul className="inner-ul">
-                                                {this.state.cadeiras.ano_2.semestre_2.map(cadeira =>
-                                                    <CadeiraNavbarItem cadeira={cadeira}/>)}
-                                                </ul>
-                                                : ""}
-                                    </ul>
-                                </ul>}
-                        </ul>
-                        <ul className="year-ul">
-                            <li onClick={() => this.handle_tab('y3')}>3ºano</li>
-                            {(this.state.display_tabs.y3) &&
-                                <ul>
-                                    <ul className="semester-ul">
-                                        <li onClick={() => this.handle_tab('y3s1')}>1º semestre</li>
-                                            {(this.state.display_tabs.y3s1) ?
+            <div id="navbar"
+                style={{"height": (this.state.display_menu) ? "calc(100% - 20px)" : "50px"}}>
+
+                <div className="nav-buttons"
+                    style={{"display": (this.state.display_menu) ? "block" : "none"}}>
+
+                    <ul className="outer-ul">
+                        <li onClick={()=>{window.location="/cadeiras"}}>Cadeiras</li>
+                            <ul className="year-ul">
+                                <li onClick={() => this.handle_tab('y1')}>1ºano</li>
+                                {(this.state.display_tabs.y1) ?
+                                    <ul>
+                                        <ul className="semester-ul">
+                                            <li onClick={() => this.handle_tab('y1s1')}>1º semestre</li>
+                                            {(this.state.display_tabs.y1s1) ?
                                             <ul className="inner-ul">
-                                            {this.state.cadeiras.ano_3.semestre_1.map(cadeira =>
+                                                {this.state.cadeiras.ano_1.semestre_1.map(cadeira => 
+                                                    <CadeiraNavbarItem cadeira={cadeira}/>)}
+                                            </ul>
+                                            : ""}
+                                        </ul>
+                                        <ul className="semester-ul">
+                                            <li onClick={() => this.handle_tab('y1s2')}>2º semestre</li>
+                                            {(this.state.display_tabs.y1s2) ?
+                                            <ul className="inner-ul">
+                                            {this.state.cadeiras.ano_1.semestre_2.map(cadeira => 
                                                 <CadeiraNavbarItem cadeira={cadeira}/>)}
                                             </ul>
                                             : ""}
+                                        </ul>
                                     </ul>
-                                    <ul className="semester-ul">
-                                        <li onClick={() => this.handle_tab('y3s2')}>2º semestre</li>
-                                            {(this.state.display_tabs.y3s2) ?
-                                            <ul className="inner-ul">
-                                            {this.state.cadeiras.ano_3.semestre_2.map(cadeira => 
-                                                <CadeiraNavbarItem cadeira={cadeira}/>)}
-                                            </ul>
-                                            : ""}
-                                    </ul>
-                                </ul>}
-                        </ul>
-                    {/* {this.state.cadeiras.map(cadeira => 
-                        <li onClick={() => console.log(cadeira.id)}>
-                            {cadeira.nome}
-                            <div className="navbar-underline"></div>
-                        </li>)} */}
-                </ul>
-                <ul className="outer-ul">
+                                : ""}
+                                
+                            </ul>
+                            <ul className="year-ul">
+                                <li onClick={() => this.handle_tab('y2')}>2ºano</li>
+                                {(this.state.display_tabs.y2) &&
+                                    <ul>
+                                        <ul className="semester-ul">
+                                            <li onClick={() => this.handle_tab('y2s1')}>1º semestre</li>
+                                                {(this.state.display_tabs.y2s1) ?
+                                                    <ul className="inner-ul">
+                                                    {this.state.cadeiras.ano_2.semestre_1.map(cadeira => 
+                                                        <CadeiraNavbarItem cadeira={cadeira}/>)}
+                                                    </ul>
+                                                    : ""}
+                                        </ul>
+                                        <ul className="semester-ul">
+                                            <li onClick={() => this.handle_tab('y2s2')}>2º semestre</li>
+                                                {(this.state.display_tabs.y2s2) ?
+                                                    <ul className="inner-ul">
+                                                    {this.state.cadeiras.ano_2.semestre_2.map(cadeira =>
+                                                        <CadeiraNavbarItem cadeira={cadeira}/>)}
+                                                    </ul>
+                                                    : ""}
+                                        </ul>
+                                    </ul>}
+                            </ul>
+                            <ul className="year-ul">
+                                <li onClick={() => this.handle_tab('y3')}>3ºano</li>
+                                {(this.state.display_tabs.y3) &&
+                                    <ul>
+                                        <ul className="semester-ul">
+                                            <li onClick={() => this.handle_tab('y3s1')}>1º semestre</li>
+                                                {(this.state.display_tabs.y3s1) ?
+                                                <ul className="inner-ul">
+                                                {this.state.cadeiras.ano_3.semestre_1.map(cadeira =>
+                                                    <CadeiraNavbarItem cadeira={cadeira}/>)}
+                                                </ul>
+                                                : ""}
+                                        </ul>
+                                        <ul className="semester-ul">
+                                            <li onClick={() => this.handle_tab('y3s2')}>2º semestre</li>
+                                                {(this.state.display_tabs.y3s2) ?
+                                                <ul className="inner-ul">
+                                                {this.state.cadeiras.ano_3.semestre_2.map(cadeira => 
+                                                    <CadeiraNavbarItem cadeira={cadeira}/>)}
+                                                </ul>
+                                                : ""}
+                                        </ul>
+                                    </ul>}
+                            </ul>
+                    </ul>
+                    <ul className="outer-ul">
                     <li onClick={() => {window.location = "profs"}}>Professores</li>
                     <ul className="inner-ul">
                         {this.state.profs.map(prof => 
@@ -176,6 +180,21 @@ class Navbar extends React.Component{
                             </li>)}
                     </ul>
                 </ul>
+                </div>
+
+                <div className="mobile-menu-button" onClick={()=>{
+                    const {display_menu} = this.state
+                    this.setState({
+                        display_menu: !display_menu
+                    })
+                }}>
+                    {(this.state.display_menu ?
+                        <MdClear size="20" color="#eeeeeeec"/>
+                    :
+                        <FiMenu size="20" color="#eeeeeeec"/>
+                    )}
+
+                </div>
             </div>
         )
     }
